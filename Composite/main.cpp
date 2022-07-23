5ea96f58c5ddaaeffe63b8615c63e26581cce563
steve <steve1414142001@gmail.com>
2022-07-23 16:03:07 +0800

    fix leak

--- Composite/main.cpp
+++ Composite/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <list>
 #include <string>
+#include <memory>
 
 #include "component.h"
 
@@ -11,21 +12,18 @@ int main() {
     Operation op_plus('+');
     Operation op_minus('-');
 
-    // should be (1+2) + (3-4)
+    std::unique_ptr<Component> leaf_1(new Leaf(1));
+    std::unique_ptr<Component> leaf_2(new Leaf(2));
+    std::unique_ptr<Component> branch1(new Composite(&op_plus, leaf_1.get(), leaf_2.get()));
 
-    Component *leaf_1 = new Leaf(1);
-    Component *leaf_2 = new Leaf(2);
-    Component *branch1 = new Composite(&op_plus, leaf_1, leaf_2);
+    std::unique_ptr<Component> leaf_3(new Leaf(3));
+    std::unique_ptr<Component> leaf_4(new Leaf(4));
+    std::unique_ptr<Component> branch2(new Composite(&op_minus, leaf_3.get(), leaf_4.get()));
 
-    Component *leaf_3 = new Leaf(3);
-    Component *leaf_4 = new Leaf(4);
-    Component *branch2 = new Composite(&op_minus, leaf_3, leaf_4);
-
-    Component *tree = new Composite(&op_plus, branch1, branch2);
+    std::unique_ptr<Component> tree(new Composite(&op_plus, branch1.get(), branch2.get()));
     
     std::cout << "b1 : " << branch1->operation() << std::endl;
     std::cout << "b2 : " << branch2->operation() << std::endl;
     std::cout << "tree : " << tree->operation() << std::endl;
-
     return 0;
 }
